ef14e57ed86b1c3e0f588d4e3eaadd6f5b33c23c
matsujirushi <matsujirushi@live.jp>
2025-01-30 06:27:50 +0000

    Deploying to gh-pages from @ SeeedJP/wio_cellular@7a0a871df90acbbff327385fae31db4b7b17f740 🚀

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['searchaccesstechnology_0',['searchAccessTechnology',['../classwiocellular_1_1network_1_1Bg770aNetwork.html#a1f29aaa3fc9cc5d652ca83ad047187ca',1,'wiocellular::network::Bg770aNetwork']]],
-  ['serverid_1',['serverId',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2_1_1SocketStatus.html#a10a6c2d87324f067f5f29548d6c064e9',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2::SocketStatus']]],
-  ['servicetype_2',['serviceType',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2_1_1SocketStatus.html#abadd6a462d4fa02a6874333cce2dd78e',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2::SocketStatus']]],
-  ['socketstate_3',['socketState',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2_1_1SocketStatus.html#a891bbf5eff67c24c91e48ab712df5a8f',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2::SocketStatus']]],
-  ['state_4',['state',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aPacketDomainCommands_1_1PdpContextStatus.html#af9f3d1b9dba9600c7ac0d577bc931f46',1,'wiocellular::module::bg770a::commands::Bg770aPacketDomainCommands::PdpContextStatus']]]
+  ['send_5fsocket_5fsize_5fmax_1',['SEND_SOCKET_SIZE_MAX',['../classwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2.html#a0020fd8e2163c93c0202d3d4acf9d4ed',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2']]],
+  ['serverid_2',['serverId',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2_1_1SocketStatus.html#a10a6c2d87324f067f5f29548d6c064e9',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2::SocketStatus']]],
+  ['servicetype_3',['serviceType',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2_1_1SocketStatus.html#abadd6a462d4fa02a6874333cce2dd78e',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2::SocketStatus']]],
+  ['socketstate_4',['socketState',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aTcpipCommands2_1_1SocketStatus.html#a891bbf5eff67c24c91e48ab712df5a8f',1,'wiocellular::module::bg770a::commands::Bg770aTcpipCommands2::SocketStatus']]],
+  ['state_5',['state',['../structwiocellular_1_1module_1_1bg770a_1_1commands_1_1Bg770aPacketDomainCommands_1_1PdpContextStatus.html#af9f3d1b9dba9600c7ac0d577bc931f46',1,'wiocellular::module::bg770a::commands::Bg770aPacketDomainCommands::PdpContextStatus']]]
 ];
